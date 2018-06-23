f6e39a4309cb8a15daf1ddf3a2f7ff808ac2c27e
森一起 <kazuki@morikazuki-no-MacBook-Air.local>
2018-06-23 21:31:59 +0900

    add new file

---
+++ opencv1.py
@@ -0,0 +1,12 @@
+import cv2
+
+capture = cv2.VideoCapture(0)
+
+while(True):
+    ret, frame = capture.read()
+    cv2.imshow('frame',frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+capture.release()
+cv2.destroyAllWindows()
